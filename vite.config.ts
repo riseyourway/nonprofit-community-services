c0d5697e4222ed34e178aa4464bc67130467cbf4
Samuel Mccarthy <riseyourway.com@gmail.com>
2025-12-19 14:00:06 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/nonprofit-community-services/", // Added base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
